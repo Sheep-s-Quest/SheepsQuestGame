1c727f893069f89e37f83fdba1a7d6a0cf7a5adf
Artem <artem.naumov.dev@gmail.com>
2024-05-23 18:14:37 +0300

    Tilemap improvements
    -Added decorations
    -Added Y-Sorting for scenery
    -Improved code for managing words of level collisions

---
+++ levels/tile_map.gd
@@ -0,0 +1,22 @@
+extends TileMap
+
+var _layers_dict = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_set_layers()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func get_layer_id_by_name(name):
+	return _layers_dict[name]
+
+
+func _set_layers():
+	for layer_id in range(self.get_layers_count()):
+		var layer_name = self.get_layer_name(layer_id)
+		_layers_dict[layer_name] = layer_id
