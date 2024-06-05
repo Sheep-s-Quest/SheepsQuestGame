8379a0edf42e6c42d31791bb862b43344db4271d
Artem <artem.naumov.dev@gmail.com>
2024-06-05 18:44:40 +0300

    Scene switching is configured

--- levels/story.gd
+++ levels/story.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _ready():
 	anim_player.play("story")
+	
+func _process(delta):
+	if(!anim_player.is_playing()):
+		get_tree().change_scene_to_file("res://levels/test_level.tscn")
